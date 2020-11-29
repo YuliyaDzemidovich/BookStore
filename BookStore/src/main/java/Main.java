9f79ff11e28fd0681cca7d53d04c2aa17e0fc624
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2020-11-29 22:18:01 +0300

    added log4j logging

--- BookStore/src/main/java/Main.java
+++ BookStore/src/main/java/Main.java
@@ -1,7 +1,12 @@
+import org.apache.log4j.Logger;
+
 public class Main {
+    final static Logger log = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
         Main m = new Main();
         System.out.println(m.getTestStr());
+        log.info("logger test");
     }
 
     public String getTestStr(){
