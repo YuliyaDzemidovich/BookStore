e812c71a81f38fda760e040821104fd9032a28e5
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2020-11-29 21:05:23 +0300

    added Spring framework support

---
+++ BookStore/src/main/java/TempBean.java
@@ -0,0 +1,5 @@
+public class TempBean {
+    public String greet(){
+        return "hello from temp bean";
+    }
+}
---
+++ BookStore/src/test/java/SpringTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+    @Test
+    void getBean() {
+        String shouldBe = "hello from temp bean";
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        TempBean bean = (TempBean) context.getBean("tempBean");
+
+        Assertions.assertEquals(bean.greet(), shouldBe);
+    }
+}
